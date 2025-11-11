2fdc447d6db8fb69ba87f244a5b03e48d289dbfb
mpitfield <mylespitfield10@outlook.com>
2025-11-11 22:58:31 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function loadbellaboo() {
-    document.getElementById('opening-screen').style.top = '-100%';
+    document.getElementById('opening-screen').style.top = '-75%';
     setTimeout(() => {
         document.getElementById('opening-screen').remove();
     }, 400);
